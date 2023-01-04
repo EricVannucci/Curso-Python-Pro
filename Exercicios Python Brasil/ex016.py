373e235d8731dff9fa7847a851983828af257ce9
Eric Vannucci <104632474+EricVannucci@users.noreply.github.com>
2023-01-04 09:19:31 -0300

    atualização

---
+++ Exercicios Python Brasil/ex016.py
@@ -0,0 +1,4 @@
+print("<<< \033[31mCalculadora de tintas\033[m >>>\n")
+h = float(input("Informe a medida em metros da altura da parede: "))
+l = float(input("Informe a medida em metros da largura da parede: "))
+m2 = h*l
