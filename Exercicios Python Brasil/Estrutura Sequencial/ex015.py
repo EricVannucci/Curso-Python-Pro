d937950975e8233583ad69d8066541ba5ca4fbc3
Eric Vannucci <104632474+EricVannucci@users.noreply.github.com>
2023-04-12 09:28:24 -0300

    ...

--- Exercicios Python Brasil/Estrutura Sequencial/ex015.py
+++ Exercicios Python Brasil/Estrutura Sequencial/ex015.py
@@ -16,10 +16,10 @@ print("<<< \033[95mCalculadora de Salario Líquido\033[m >>>\n")
 salario_hora = float(input("Informe o valor da Hora/Trabalho: R$"))
 hora_trabalhada = float(input("Informe quantas horas foram trabalhadas no mês: "))
 salario_bruto = salario_hora*hora_trabalhada
-ir = salario_bruto*11/100
-inss = salario_bruto*8/100
-sind = salario_bruto*5/100
-salario_liquido = salario_bruto-(ir+inss+sind)
+ir = salario_bruto * 11 / 100
+inss = salario_bruto * 8 / 100
+sind = salario_bruto * 5 / 100
+salario_liquido = salario_bruto - (ir + inss+sind)
 print(f"\nSalário Bruto Mensal: R${salario_bruto:.2f}")
 print(f"Desconto IR (11%): R${ir:.2f}")
 print(f"Desconto INSS (8%): R${inss:.2f}")
